324f068712e17b61f2fac3348cbb62b68319a1c5
rbkv-dev <rbkv.dev@gmail.com>
2020-07-15 10:57:24 +0300

    add pow function

--- test.js
+++ test.js
@@ -3,5 +3,18 @@ function Add(a) {
     return a + b;
   };
 }
+console.log("Add", Add(2)(3)); // 5
 
-let res = Add(2)(3); // 5
+function pow(num, i) {
+  if (i == 1) return num;
+  return num * pow(num, --i);
+}
+console.log("pow", pow(2, 3)); // 8
+
+function newPow(num) {
+  return function newPow(i) {
+    if (i == 1) return num;
+    return num * pow(num, --i);
+  };
+}
+console.log("newPow", newPow(2)(3)); // 8
